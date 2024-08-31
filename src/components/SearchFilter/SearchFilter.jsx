19fef704d0aede3cbd9a1e1a074ae51c73071af2
Gaming <alperenilgaz3@gmail.com>
2024-09-01 02:14:06 +0300

    search filter compenent created

---
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchFilter.css'
+
+const SearchFilter = () => {
+  return (
+    <div className="search-filter">
+        <input type="text" placeholder='yarışma ara...' />
+    </div>
+  )
+}
+
+export default SearchFilter
--- src/pages/Education/EducationPage.jsx
+++ src/pages/Education/EducationPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import EducationCard from '../../components/EducationCard/EducationCard'
+import SearchFilter from '../../components/SearchFilter/SearchFilter'
+
 
 const EducationPage = () => {
   return (
     <>
+     
+        <SearchFilter/>
         <EducationCard/>
      
        
