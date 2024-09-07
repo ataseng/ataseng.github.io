a97c0e755dcba6abea829fbc81627cb29dfde5b0
zumrudu-anka <osmandurdag@hotmail.com>
2024-09-08 01:04:37 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from 'react';
 import Home from './pages/Home/Home'
-function App() {
-  const getStoredTheme = localStorage.getItem("theme") || ""
+import Sidebar from './components/Sidebar/Sidebar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-  const [theme, setTheme] = useState(getStoredTheme)
+function App() {
   
-  const changeTheme = (newTheme) => {
-    const previousTheme = localStorage.getItem("theme")
-    if(previousTheme){
-      document.body.classList.remove(previousTheme)
-    }
-      if(newTheme){
-        document.body.classList.add(newTheme)
-        localStorage.setItem("theme",newTheme)
-      }
-    
-  }
-  useEffect(() => {
-      changeTheme(theme)
-  },[theme])
   return (
-    <>
-    <div className="theme-btn">
-      <button onClick={() =>setTheme(theme==="dark" ? "light" : "dark")}>{theme}</button>
-    </div>
-      <Home/>
-    </>
+    <Router>
+        <Sidebar />
+        <Home/>
+    </Router>
   );
 }
 
