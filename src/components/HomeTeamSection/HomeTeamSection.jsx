7f2874dddda5bbb714c72d685eac8c7791e717bd
Gaming <alperenilgaz3@gmail.com>
2024-10-20 23:15:10 +0300

    HomePage billboard & team section completed

--- src/components/HomeTeamSection/HomeTeamSection.jsx
+++ src/components/HomeTeamSection/HomeTeamSection.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
-import './HomeTeamSection'
-
+import './homeTeamSection.css'
+import PersonImage from '../../assets/images/personImage.jpg'
+import Data from './HomeTeam.json'
 const HomeTeamSection = () => {
   return (
-    <div>HomeTeamSection</div>
+    <>
+      <div className="sidebar-margin">
+        <div className="team-section">
+          <div className="team-section-title">
+            <div className="left-line"></div>
+            <h2>EKİBİMİZ</h2>
+            <div className="right-line"></div>
+          </div>
+          <div className="team-section-card-wrap">
+
+            {
+              Data.map((item, key) => (
+                <div key={key} className="team-section-card">
+                  <div className="person-image">
+                    <img src={PersonImage} alt="" />
+                  </div>
+                  <div className="person-name">
+                    <p>{item.name}</p>
+                  </div>
+
+                  <div className="departmant-info">
+                    <div className="departmant">
+                      <p>{item.departmant}</p>
+                    </div>
+                    <div className="grade">
+                      <p>{item.grade}</p>
+                    </div>
+                  </div>
+
+                  <div className="club-positions">
+                    <p>{item.clubPosition}</p>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+          <div className="team-section-link">
+            <a href="#">Diğer ekip üyleri için <span>”Ekibimiz”</span> sayfasını ziyaret edin</a>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,16 @@ import './Home.css';
 import AboutSection from '../../components/AboutSection/AboutSection';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import HomeBillboardSection from '../../components/HomeBillboardSection/HomeBillboardSection';
+import HomeTeamSection from '../../components/HomeTeamSection/HomeTeamSection';
 
 const Home = () => {
   return (
     <>
      
       <WelcomeSection/>
-      <HomeBillboardSection/>
       <AboutSection />
+      <HomeBillboardSection/>
+      <HomeTeamSection/>
     </>
   );
 }
