daa673a99de1da5f0cc08ae2bb85680f60b70a3e
Gaming <alperenilgaz3@gmail.com>
2024-10-21 03:22:46 +0300

    homepage feedback section completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 import HomeBillboardSection from '../../components/HomeBillboardSection/HomeBillboardSection';
 import HomeTeamSection from '../../components/HomeTeamSection/HomeTeamSection';
 import HomeDiscordSection from '../../components/HomeDiscordSection/HomeDiscordSection';
+import HomeFeedBackSection from '../../components/HomeFeedBackSection/HomeFeedBackSection';
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
       <HomeBillboardSection/>
       <HomeTeamSection/>
       <HomeDiscordSection/>
+      <HomeFeedBackSection/>
     </>
   );
 }
