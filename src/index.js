9b01b648284923a94868954faa0a350c5f2b4a72
zumrudu-anka <osmandurdag@hotmail.com>
2025-07-24 14:41:51 +0300

    .

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <MainLayout>
-                    <App />
-                </MainLayout>
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <BrowserRouter>
+            <MainLayout>
+                <App />
+            </MainLayout>
+        </BrowserRouter>
+    </Provider>
 );
